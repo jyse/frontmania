89fdd538c3bb76bea417500c39726573f967fb5f
Jessy The <jessythe@jessys-mbp-2.kpn>
2022-10-04 00:26:42 +0200

    fully Frontend Part 2 added from scratch and collection in OpenSea visible

--- Test.js
+++ Test.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import StartMinting from "./components/StartMinting";
 import InProgressMinting from "./components/InProgressMinting";
@@ -15,15 +15,20 @@ function App() {
   const [account, setAccount] = useState();
   const [contract, setContract] = useState();
   const [supply, setSupply] = useState(0);
-  const [hash, setHash] = useState();
+  const [hash, setHash] = useState("");
 
   const mint = async () => {
-    const payload = { value: ethers.utils.parseEther("0.01") };
-    const transaction = await contract.safeMint(payload);
-    console.log(transaction.hash, "🧾 transaction hash");
+    // Step 4: Write the mint function
+    const options = { value: ethers.utils.parseEther("0.01") };
+    const transaction = await contract.safeMint(1, options);
+    console.log(transaction, "logging transaction");
     setHash(transaction.hash);
 
     // Step 7: Set the variables for progress and completed
+    // setInProgress(true)
+    //  await transaction.wait()
+    //  setInProgress(false)
+    //  setCompleted(true)
     setInProgress(true);
     await transaction.wait();
     setInProgress(false);
@@ -31,34 +36,40 @@ function App() {
   };
 
   const getTotalSupply = async () => {
+    // Step 3: Add totalSupply and use it from the contract
     const totalSupply = await contract.totalSupply();
     setSupply(totalSupply.toNumber());
   };
 
+  // Step 3: Contract => getTotalSupply()
   useEffect(() => {
+    console.log(contract, " what is contract here? 📝🔥");
     if (contract) {
       getTotalSupply();
     }
   }, [contract]);
 
   const login = async () => {
+    // Step 1: Connect wallet (check Metamask + accounts)
+
+    console.log("logging in");
     if (typeof window.ethereum !== "undefined") {
-      console.log("Yayy Metamask is installed");
+      console.log("Metamask is installed!");
+      window.ethereum.request({ method: "eth_requestAccounts" });
 
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts"
       });
       const walletAccount = accounts[0];
-      console.log(walletAccount, "🦊 Metamask account!");
       setAccount(walletAccount);
 
-      const contractAddress = "0x380A66B9aE2ABBc41d546AF0482A18f91B226d0b";
-
+      // Step 2: Wire up contract (provider, signer, NFTContract)
+      // ❌👇
+      const contractAddress = "0x3bd5d6612ed5406e310ef366de1fa4354d001281";
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner(walletAccount);
 
       let NFTContract = new ethers.Contract(contractAddress, abi, signer);
-      console.log(NFTContract, "📝 NFT Contract");
       setContract(NFTContract);
     }
   };
@@ -72,13 +83,12 @@ function App() {
     if (completed) {
       return <CompletedMinting />;
     }
-
     // Step 6: Pass mint as props
     return <StartMinting mint={mint} />;
   };
 
   return (
-    <div className="pp">
+    <div className="app">
       <Header />
       <div className="hero">
         <video className="bg-video" loop autoPlay muted>
@@ -92,25 +102,25 @@ function App() {
             <div className="information">
               <div className="information-header-container">
                 <h4 className="information-subheader">
-                  1st Frontmania NFT collection:
+                  1st Frontmania NFT Collection:
                 </h4>
                 <h1 className="information-header">Frontmaniacs </h1>
               </div>
               <div className="information-interactions-container">
-                <p> {supply} / 1000 minted </p>
-                {/* // Step 5: insert getState() */}
+                <p> {supply} / 1000 minted</p>
+                {/* Step 5: insert getState() */}
                 {account ? (
                   getState()
                 ) : (
                   <div className="button" onClick={login}>
-                    Connect Wallet
+                    <h2>CONNECT WALLET</h2>
                   </div>
                 )}
               </div>
             </div>
           </div>
           <div className="footer">
-            <h2>Mint a NFT from FRONTMANIACS now!</h2>
+            <h1>Mint a NFT from FRONTMANIACS now!</h1>
           </div>
         </div>
       </div>
--- frontmaniacs-minting-site/src/components/CompletedMinting.js
+++ frontmaniacs-minting-site/src/components/CompletedMinting.js
@@ -3,7 +3,7 @@ import React from "react";
 const CompletedMinting = () => {
   const viewOpenSea = () => {
     // View your collection on Testnet OpenSea (www.testnets.opensea.io)
-    const url = "";
+    const url = "https://testnets.opensea.io/collection/frontmania";
     window.open(url, "_blank");
   };
 
